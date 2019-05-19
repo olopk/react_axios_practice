1c1c604eca76d9d3ad466e54eddcff8800ea59cb
olopk <olekwojas@gmail.com>
2019-05-19 17:59:29 +0200

    working with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
 const App = () => (
+  <BrowserRouter>
       <div className="App">
         <Blog />
       </div>
+  </BrowserRouter>   
     );
 
 export default App;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom'; 
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        clicked: null
-    }
-    
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            const data = response.data.slice(0,4);
-            const upData = data.map(post => {
-                return{
-                ...post,
-                author: 'ololek'
-                }
-            })
-            this.setState({posts: upData})
-        })
-    }
-    
-    clickedHandler(id) {
-        this.setState({clicked: id})
-    }
-
-
     render () {
-
-        const posts = this.state.posts.map(post => (
-            <Post key={post.id} title={post.title} author={post.author} clicked={() => this.clickedHandler(post.id)}/>
-        ));
-
         return (
             <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost check={this.state.clicked}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <header>
+                    <nav>
+                        <ul className="List">
+                            <li><NavLink to="/" exact>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" exact component={NewPost}/>
             </div>
         );
     }
